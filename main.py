5ad4a67acd41bc0aa8bc42d02363343f94cacfa1
Saranpat Pattasarankul <ohmohm102549@gmail.com>
2025-01-15 22:37:48 +0700

    import

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.togglebutton import ToggleButton
+
+class Paddle(Widget):
+    score = NumericProperty(0)
 class Ball(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
