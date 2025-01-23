b546db28a2ea5bf2552887d35976fa81488804a1
Saranpat Pattasarankul <ohmohm102549@gmail.com>
2025-01-23 14:54:01 +0700

    fix condent

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.gridlayout import Gridlayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.togglebutton import ToggleButton
@@ -27,7 +27,7 @@ class VollyeballGame(Widget):
     player2 = ObjectProperty(None)
     status_label = ObjectProperty(None)
 class menu(Boxlayout):
-        def __init__(self, call_gamestart, **kwargs):
+    def __init__(self, call_gamestart, **kwargs):
         super().__init__(**kwargs)
         self.orientation = 'vertical'
         self.call_gamestart = call_gamestart
